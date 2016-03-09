88547aad81f74abecf7f9ce69ca211b937dd4146
lucho <lhitaliani@gmail.com>
2016-03-09 06:47:35 -0300

    Agregue sistema de base de datos

---
+++ bin/db.py
@@ -0,0 +1,20 @@
+import sqlite3
+
+crearDb = sqlite3.connect('myweb.db')
+query = crearDb.cursor()
+
+def crearTable():
+    query.execute('''CREATE TABLE cliente (id INTEGER PRIMARY KEY, nombre TEXT)''')
+
+def agregarCliente(nomb):
+    query.execute("INSERT INTO cliente (id, nombre) VALUES (?,?)",(1,nomb))
+
+def main():
+    crearTable()
+    agregarCliente('horacio')
+    crearDb.commit()
+
+    query.execute('SELECT * FROM cliente')
+
+if __name__ == '__main__':
+    main()
--- test/test_bin.py
+++ test/test_bin.py
@@ -23,3 +23,65 @@ class TestNavegacion(CasoTest):
         self.assert_status_OK(request.status)
         resp = 'Viva la senior'
         self.assert_es_pagina(request.data, resp)
+
+    def test_registrar_GET(self):
+        request = app.request('/registrar')        
+
+class TestUsuario(CasoTest):
+    pass
+
+class TestDataBase:
+
+    def test_seleccionar_en_db(self):
+        db = web.database(dbn='sqlite',db='myweb.db')
+
+        cliente = db.select('cliente')
+        nombre = 'horacio'
+        assert cliente.__getitem__(0).get('nombre') == nombre, 'Se esperaba que %r fuese igual a %r' % (cliente, nombre)
+
+    def test_insertar_en_db(self):
+        db = web.database(dbn='sqlite', db='myweb.db')
+
+        cliente = db.select('cliente')
+        cant_clientes = self.longitud_cliente(cliente)
+        cliente_insertar = db.insert('cliente', id=2, nombre='cuasi')
+
+        var = 'id=2'
+        cliente = db.select('cliente', where=var)
+        nombre = 'cuasi'
+        assert cliente.__getitem__(0).get('nombre') == nombre, 'Se esperaba que %r fuese igual a %r' % (cliente.__getitem__(0).get('nombre'), nombre)
+
+        cliente = db.select('cliente')
+        cliente = self.longitud_cliente(cliente)
+        assert cant_clientes != cliente, 'Se esperaba %r y se obtuvo %r.' % (cant_clientes, cliente)
+
+        self.borrar_entrada_en_db()
+
+    '''###################################################################'''
+
+    def borrar_entrada_en_db(self):
+        db = web.database(dbn='sqlite', db='myweb.db')
+        cliente = db.select('cliente')
+        cant_clientes = self.longitud_cliente(cliente)
+
+        var = 'id=2'
+        cliente = db.delete('cliente', where=var)
+
+        cliente = db.select('cliente')
+        cliente = self.longitud_cliente(cliente)
+        assert cant_clientes-1 == cliente, 'Se esperaba %r y se obtuvo %r.' % (cant_clientes, cliente)
+
+    def longitud_cliente(self,cliente):
+        cant = 0
+        for i in cliente:
+            cant += 1
+        return cant
+
+
+if __name__ == '__main__':
+    import web
+    db = web.database(dbn='sqlite',db='myweb.db')
+    cliente = db.select('cliente')
+    for i in cliente:
+        print i
+    # TestDataBase().borrar_entrada_en_db()
